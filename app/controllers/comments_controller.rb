35f1fafed6c48a55f290b24c303a1b6a0f5b79e5
JunhyeokJeon <inamorfati@naver.com>
2018-06-02 16:19:35 +0900

    add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:content))
+    @comment.nickname = current_user.nickname
+    @comment.user_id = current_user.id
+    @comment.page_user_id = @post.this_user_id
+
+    if @comment.save
+      redirect_to "/"
+    else 
+  end
 end
