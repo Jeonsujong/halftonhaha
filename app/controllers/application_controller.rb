f5277f4ae001e05a5b442a120aeabe1856c0d37c
JunhyeokJeon <inamorfati@naver.com>
2018-06-02 13:39:07 +0900

    add something

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:image1, :image2, :image3, :image4, :image5, :nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:image1, :image2, :image3, :image4, :image5, :nickname])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:image])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,15 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
 
+  has_attached_file :image1, styles: { medium: "500x500>", big: "900x900>" }
+  validates_attachment_content_type :image1, content_type: /\Aimage\/.*\z/
+  has_attached_file :image2, styles: { medium: "500x500>", big: "900x900>" }
+  validates_attachment_content_type :image2, content_type: /\Aimage\/.*\z/
+  has_attached_file :image3, styles: { medium: "500x500>", big: "900x900>" }
+  validates_attachment_content_type :image3, content_type: /\Aimage\/.*\z/
+  has_attached_file :image4, styles: { medium: "500x500>", big: "900x900>" }
+  validates_attachment_content_type :image4, content_type: /\Aimage\/.*\z/
+  has_attached_file :image5, styles: { medium: "500x500>", big: "900x900>" }
+  validates_attachment_content_type :image5, content_type: /\Aimage\/.*\z/
 
 end
