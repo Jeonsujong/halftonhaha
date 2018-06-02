6f799f9a5d8fe45d5c3deae606289bc355870df1
Minseok Bang <minseokbang@Minseoks-MacBook-Air.local>
2018-06-02 13:16:41 +0900

    user list

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   resources :posts
-<<<<<<< HEAD
+# <<<<<<< HEAD
 
   root "posts#index"
-=======
+# =======
   devise_for:users
-<<<<<<< HEAD
+# <<<<<<< HEAD
 
   root 'posts#index'
-=======
->>>>>>> 8bb9f976757e1b4001e31da055400df5136dbe74
->>>>>>> 506521fa8b8ee1acb51268cdf0176d4d11851689
+# =======
+# >>>>>>> 8bb9f976757e1b4001e31da055400df5136dbe74
+# >>>>>>> 506521fa8b8ee1acb51268cdf0176d4d11851689
+
+
 end
